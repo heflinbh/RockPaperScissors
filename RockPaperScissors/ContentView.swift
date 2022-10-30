08960c48fad39db55da069b586de9d5ddc8fe008
heflinbh <heflinbh@miamioh.edu>
2022-10-29 20:16:04 -0400

    Functioning game, need to work on initialization

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -7,15 +7,77 @@
 
 import SwiftUI
 
+
+
+
 struct ContentView: View {
+    
+    @State private var curAppMove = "Rock"
+    @State private var shouldWin = false
+    @State private var playerScore =  0
+    @State private var showScoreAlert = false
+    
+    private let possibleMoves = ["Rock", "Paper", "Scissors"]
+    private let winConditions = [("Rock", "Scissors"), ("Scissors", "Paper"), ("Paper", "Rock")]
+    private let loseConditions = [("Scissors", "Rock"), ("Paper", "Scissors"), ("Rock", "Paper")]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack (spacing: 30){
+            Spacer()
+            Text("Player Score: \(playerScore)")
+            Text("App's Move: \(curAppMove)")
+            Text(shouldWin ? "You should win" : "You should lose")
+            Spacer()
+            HStack {
+                ForEach(possibleMoves, id: \.self) { playerMove in
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Button {
+                        respondToTap(playerMove: playerMove)
+                    } label:{
+                        Text(playerMove == "Rock" ? "🗿" : (playerMove == "Scissors" ? "✂️" : "📄"))
+                            .font(.largeTitle)
+                    }
+                    .alert("Result", isPresented: $showScoreAlert) {
+                        Button("OK", action: playAgain)
+                    } message: {
+                        Text(
+                        """
+                        Your score is now \(playerScore)
+                        """)
+                    }
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                }
+                
+            }
+            Spacer()
         }
         .padding()
+    }
+    
+    func getRandomMove() -> String {
+        possibleMoves[Int.random(in: 0...2)]
+    }
+    
+    func respondToTap(playerMove: String) {
+        let correctWin = shouldWin && winConditions.contains(where: {$0 == (playerMove, curAppMove)})
+        let correctLose = !shouldWin && loseConditions.contains(where: {$0 == (playerMove, curAppMove)})
+        
+        if correctWin || correctLose {
+            playerScore += 1
+        }
+        else {
+            playerScore -= 1
+        }
+        showScoreAlert = true
+    }
+    
+    func playAgain() {
+        curAppMove = getRandomMove()
+        shouldWin.toggle()
     }
 }
 
@@ -24,3 +86,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
